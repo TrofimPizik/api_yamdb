d7e9aad8c0bbcfa37de064b9480a3094ab47b29c
Владислав Михалко <grm26@yandex.ru>
2023-03-16 21:40:39 +0300

    add viewsets and routes for them

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,74 +1,85 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 import datetime as dt
 
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-from reviews.models import Category, Genre, Title, Review, Genre_title, Comment
+from reviews.models import Category, Comment, Genre, Review, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'name', 'slug')
+        fields = ('name', 'slug',)
         model = Category
 
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = ('id', 'name', 'slug')
+        fields = ('name', 'slug',)
         model = Genre
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(
-        read_only=True, slug_field='name'
-    )
-    genre = GenreSerializer(required=False, many=True)
+    category = CategorySerializer(required=False)
+    genre = GenreSerializer(many=True, read_only=True)
+    rating = serializers.IntegerField(default=None)
+
     class Meta:
-        fields = ('id', 'name', 'year', 'category', 'genre')
+        fields = '__all__'
+        model = Title
+
+
+class TitleAddSerializer(serializers.ModelSerializer):
+    category = SlugRelatedField(
+        queryset=Category.objects.all(), slug_field='slug',
+    )
+    genre = SlugRelatedField(
+        queryset=Genre.objects.all(), slug_field='slug', many=True,
+    )
+    rating = serializers.IntegerField(default=None)
+
+    class Meta:
+        fields = '__all__'
         model = Title
 
     def validate_year(self, value):
         year = dt.date.today().year
-        if (year <= value):
+        if year < value:
             raise serializers.ValidationError('Проверьте год!')
         return value
 
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
-    class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
-        model = Comment
-        read_only_fields = ('author', 'post')
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        read_only=True,
         slug_field='username',
         default=serializers.CurrentUserDefault(),
-        queryset=User.objects.all()
     )
-    following = serializers.SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all()
+    title = SlugRelatedField(
+        queryset=Title.objects.all(), slug_field='name',
     )
 
     class Meta:
-        fields = ('user', 'following')
-        model = Follow
-
+        # fields = ('id', 'author', 'text', 'pub_date', 'score',)
+        fields = '__all__'
+        model = Review
         validators = [
             UniqueTogetherValidator(
-                queryset=Follow.objects.all(),
-                fields=('user', 'following')
+                queryset=Review.objects.all(),
+                fields=('author', 'title'),
+                message=(
+                    'Нельзя оставлять оценку дважды '
+                    'на одного и тот же фильм.'
+                ),
             )
         ]
 
-    def validate(self, data):
-        if data['user'] == data['following']:
-            raise serializers.ValidationError(
-                'Нельзя подписаться на самого себя')
-        return data
+
+class CommentSerializer(serializers.ModelSerializer):
+    review = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = SlugRelatedField(read_only=True, slug_field='username',)
+
+    class Meta:
+        # fields = ('id', 'author', 'text', 'pub_date', 'review')
+        fields = '__all__'
+        model = Comment

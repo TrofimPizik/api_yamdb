381774fdfb76224df765a9775c0914d97c3d01ea
Владислав Михалко <grm26@yandex.ru>
2023-03-17 18:15:23 +0300

    add permissions to the project

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,1 +1,31 @@
+from rest_framework import permissions
 
+
+class AuthorAccess(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
+
+
+class ModeratorAccess(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'moderator'
+        )
+
+
+class AdminAccess(permissions.BasePermission):
+    def has_permission(self, request, view,):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'admin'
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'admin'
+        )
